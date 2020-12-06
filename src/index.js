b2a2d2393dcc1a667bbcf1c224808c7d7a31b355
Prabhudas-M <60952658+Prabhudas-M@users.noreply.github.com>
2020-12-06 08:48:31 -0500

    1

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+const FatherObj = {
+  name:"prabhudas",
+age:47
+}
+
+const Mother ={
+...FatherObj,
+name : "Suganthi",
+age:45
+}
+
+console.log(Mother);
+
 ReactDOM.render(<App />, document.getElementById("root"));
